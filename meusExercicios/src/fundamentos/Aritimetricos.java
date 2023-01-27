78372f1623653142b551239f73d34b5a559de676
maikeg-alves <maicongabrielalves99@gmai.com>
2023-01-27 18:42:33 -0300

    Aritmetricos

---
+++ meusExercicios/src/fundamentos/Aritimetricos.java
@@ -0,0 +1,24 @@
+package fundamentos;
+
+public class Aritimetricos {
+    public static void main(String[] args) {
+        System.out.println(2 + 3);
+
+        var x = 34.56;
+        double y = 2.2;
+
+        System.out.println("Adição " + (x + y));
+        System.out.println("Subtração " + (x - y));
+        System.out.println("Multiplicação " + (x * y));
+        System.out.println("Divisão " + (x / y));
+
+        int a = 8;
+        int b = 3;
+
+        System.out.println("Adição " + (a + b));
+        System.out.println("Subtração " + (a - b));
+        System.out.println("Multiplicação " + (a * b));
+        System.out.println("Divisão " + (a / b));
+
+    }
+}
