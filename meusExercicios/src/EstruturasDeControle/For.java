d0c10bb16571d7d5e41f761e11e0519e350b328f
maikeg-alves <maicongabrielalves99@gmai.com>
2023-01-31 16:51:28 -0300

    Estrutura de Controle: for

---
+++ meusExercicios/src/EstruturasDeControle/For.java
@@ -0,0 +1,24 @@
+package EstruturasDeControle;
+
+public class For {
+    public static void main(String[] args) {
+
+        int contador = 10;
+
+        for(int i = 0; i <= contador; i++) {
+           System.out.println("Bom dia!");
+        }
+        
+        int x = 2;
+        for (;x < 10;) {
+          System.out.println("x = " + x);
+          x++;
+        }
+
+        //loop infinito 
+
+        for(;true;) {
+           System.out.println("infinito oo");
+        }
+    }
+}
