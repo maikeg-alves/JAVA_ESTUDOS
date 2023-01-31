58386c8a3ba910acb3b0adb419ce71c7d3a8b464
maikeg-alves <maicongabrielalves99@gmai.com>
2023-01-31 16:42:18 -0300

    Estrutura de Controle: while

---
+++ meusExercicios/src/EstruturasDeControle/While.java
@@ -0,0 +1,13 @@
+package EstruturasDeControle;
+
+public class While {
+    public static void main(String[] args) {
+
+        int contador = 1;
+
+        while (contador <= 10) {
+            System.out.println("Bom dia!");
+            contador++;
+        }
+    }
+}
